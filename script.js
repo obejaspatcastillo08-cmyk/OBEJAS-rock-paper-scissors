c623a869986665d679fae07ea3145fa989c406c3
Patricia kate Obejas <obejaspatcastillo08@gmail.com>
2026-04-25 04:18:45 -0300

    Added Rock-Paper-Scissors game function with emoji support and scoring system

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let userScore=0;
 let computerScore=0;
+
+function play(userchoice){
+  const choices=["rock","paper","scissors"];
+  const emojis={
+    rock:"✊",
+    paper:"✋",
+    scissors:"✌️"  
+  };
+}
