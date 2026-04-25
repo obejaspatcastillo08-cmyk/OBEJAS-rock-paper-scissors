32d7f167c8774a1c3e7a0d4fc378f4167fe3f678
Patricia kate Obejas <obejaspatcastillo08@gmail.com>
2026-04-25 04:33:08 -0300

    Add game logic to determine win, lose, or draw result

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ const computerChoice=choices[Math.floor(Math.random()*3)];
 document.getElementById("user-choice").textContent=emojis[userChoice];
 document.getElementById("computer-choice").textContent=emojis[computerChoice];
 
-let resilt="";
+let result="";
+
+if (userChoice == computerChoice) {
+  result = "It's a Draw!";
+} else if (
+  (userChoice == "rock" && computerChoice == "scissors") ||
+  (userChoice == "paper" && computerChoice == "rock") ||
+  (userChoice == "scissors" && computerChoice == "paper")
+) {
+  result = "You Win!";
+} else {
+  result = "Computer Wins!";
+}
 
 
